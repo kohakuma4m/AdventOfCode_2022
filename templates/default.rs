fd885af55a392dc2d7b96c4d03a202ec3c0d8c2e
Marc-André Malouin <mamalouin@spiria.com>
2022-12-02 01:27:44 -0500

    Initial setup

---
+++ templates/default.rs
@@ -0,0 +1,15 @@
+pub fn solution1(data: String) {
+    println!("{}", data);
+
+    println!("=========================");
+    println!("Solution1: ");
+    println!("=========================");
+}
+
+pub fn solution2(data: String) {
+    println!("{}", data);
+
+    println!("=========================");
+    println!("Solution2: ");
+    println!("=========================");
+}
