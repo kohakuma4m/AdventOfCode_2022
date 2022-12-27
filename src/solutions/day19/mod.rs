e23fad769d69655ebb7802a91e43858e1d76e08a
Marc-André Malouin <mamalouin@spiria.com>
2022-12-26 23:19:40 -0500

    Day 19 solution

--- src/solutions/day19/mod.rs
+++ src/solutions/day19/mod.rs
@@ -19,12 +19,21 @@ pub fn solution1(data: String) -> usize {
     result
 }
 
-pub fn solution2(data: String) {
-    let blueprints = read_blueprints(data);
+pub fn solution2(data: String) -> usize {
+    let mut blueprints = read_blueprints(data).into_iter().filter(|b| b.id <= 3).collect::<Vec<Blueprint>>();
+
+    for blueprint in blueprints.iter_mut() {
+        blueprint.print();
+        analyze_blueprint(blueprint, 32);
+    }
+
+    let result = blueprints.iter().map(|b| b.quality_level / b.id as usize).product();
 
     println!("=========================");
-    println!("Solution2: ");
+    println!("Solution2: {result}");
     println!("=========================");
+
+    result
 }
 
 /////////////////////////////////////////////////
@@ -134,6 +143,12 @@ struct FactoryState {
     robot_creation_history: Vec<(usize, Material)>
 }
 
+impl fmt::Display for FactoryState {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "robots: {:?}, resources: {:?}, time_left: {}, history: {:?}", self.robots, self.resources, self.time_remaining, self.robot_creation_history)
+    }
+}
+
 impl FactoryState {
     fn new(time_remaining: usize) -> FactoryState {
         FactoryState { robots: vec![1, 0, 0, 0], resources: vec![0, 0, 0, 0], time_remaining, robot_creation_history: vec![] }
@@ -143,7 +158,7 @@ impl FactoryState {
         println!("State --> robots: {:?}, resources: {:?}", self.robots, self.resources);
         #[cfg(test)]
         for (time, material) in self.robot_creation_history.iter() {
-            println!("  > time {:2} --> {:?} robot created", time, material);
+            println!("  > time remaining {:2} --> {:?} robot created", time, material);
         }
         println!("------");
     }
@@ -183,9 +198,19 @@ impl FactoryState {
         self.robot_creation_history.push((self.time_remaining, kind));
     }
 
-    fn max_possible_geodes(&self) -> usize {
-        // The maximum number of geode possible for current state at the end if a new geode robot could be created each following turn
-        let mut max_possible_value = self.resources[Material::Geode as usize] + self.robots[Material::Geode as usize] * self.time_remaining;
+    fn geode_production_score(&self) -> (usize, usize, usize, usize) {
+        (
+            // Resource production in order of importance
+            self._max_possible_resources(Material::Geode),
+            self._max_possible_resources(Material::Obsidian),
+            self._max_possible_resources(Material::Clay),
+            self._max_possible_resources(Material::Ore)
+        )
+    }
+
+    fn _max_possible_resources(&self, kind: Material) -> usize {
+        // The maximum number of resource possible for current state at the end if a new resource robot could be created each following turn
+        let mut max_possible_value = self.resources[kind as usize] + self.robots[kind as usize] * self.time_remaining;
         for n in 1..self.time_remaining {
             max_possible_value += self.time_remaining - n // i.e: (time_left - 1)!
         }
@@ -213,85 +238,87 @@ fn build_resource_map(blueprint: &Blueprint) -> HashMap<Material, usize> {
     resources_map
 }
 
-// previous guess: 953 (too low) --> 942 ???
-
-// Depth first search solution
+// Depth first search solution (completes in less than one minute)
 fn analyze_blueprint(blueprint: &mut Blueprint, duration: usize) {
     println!("Analyzing blueprint #{}...", blueprint.id);
 
     // Building max resource quantity need to build any robot
     let resources_map = build_resource_map(blueprint);
 
-    let mut time = 0;
+    let mut best_score = (0, 0, 0, 0);
     let mut factory_states = vec![FactoryState::new(duration)];
+    let mut processed_states = vec![];
     while factory_states.len() > 0 {
-        // let max_geodes = factory_states.iter().map(|state| state.max_possible_geodes(duration - time)).max().unwrap();
-        // let mut current_states: Vec<FactoryState> = factory_states
-        //     .drain(..)
-        //     // Pruning all state which cannot produce more than the current best possible value at the end of duration...
-        //     .filter(|state| state.max_possible_geodes(duration - time) >= max_geodes)
-        //     .collect();
-
         // Current state
         let mut current_state = factory_states.pop().unwrap();
 
-        // Choose next robot to build
-        let robots_to_build = current_state.get_buildable_robots(blueprint);
+        // Choose next robot to build (There will always be at least the Geode robot, even after filtering)
+        let mut robots_to_build: Vec<Material> = current_state
+            .get_buildable_robots(blueprint)
+            .iter()
+            // Avoid building robots for resource we already produce enough to build any other robot each turn
+            .filter(|kind| {
+                match resources_map.get(*kind) {
+                    Some(max_value) => current_state.robots[**kind as usize] < *max_value,
+                    None => true // No robot need geode to build
+                }
+            })
+            .map(|kind| *kind)
+            .collect();
 
-        // Choose best robot to build, for now, we will analyze other choices later when backtracking...
-        let best_robot = robots_to_build.last().unwrap();
+        // Avoid building any other robot if we can build a geode robot right now
+        if robots_to_build.contains(&Material::Geode) && current_state.can_build_robot(blueprint, Material::Geode) == true {
+            robots_to_build = vec![Material::Geode]
+        }
 
-        for state in current_states.iter_mut() {
-            // Checking which useful robot(s) we can build with current resources (before collecting)
-            let robots_to_build: Vec<Material> = state
-                .get_robots_to_build(blueprint)
-                .iter()
-                // Avoid building robots for resource we already produce enough each turn to build any other robot each turn
-                .filter(|kind| {
-                    match resources_map.get(*kind) {
-                        Some(max_value) => state.robots[**kind as usize] < *max_value,
-                        None => true // No robot need geode to build
-                    }
-                })
-                .map(|kind| *kind)
-                .collect();
+        // Future (branches) states: one for each robot to build (the last one will always be for "best" buildable robot that we continue exploring next...)
+        let mut nb_new_states = 0;
+        for kind in robots_to_build.into_iter() {
+            let mut future_state = current_state.clone();
 
-            // Collecting resources
-            state.collect_resources();
-
-            if robots_to_build.len() == 0 || time == duration {
-                // Must wait for more resources (or not enough time) to build another robot...
-                factory_states.push(state.clone());
-                continue;
+            // Collecting resources...
+            while future_state.time_remaining > 0 && future_state.can_build_robot(blueprint, kind) == false {
+                future_state.collect_resources(); // ... until we can build chosen robot
             }
 
-            if robots_to_build.contains(&Material::Geode) {
-                // We should always prioritize building this robot if we can since it's the one we need to maximize output...
-                state.build_robot(blueprint, Material::Geode, time);
-                factory_states.push(state.clone());
-                continue;
-            }
+            if future_state.time_remaining > 0 {
+                // Collecting resources for this turn
+                future_state.collect_resources();
 
-            // Otherwise, we should either build robot or wait for more resources, unless we still don't have a clay robot ???
-            if state.robots[Material::Clay as usize] > 0 {
-                factory_states.push(state.clone());
-            }
+                // Building robot
+                future_state.build_robot(blueprint, kind);
 
-            for material in robots_to_build.into_iter() {
-                let mut new_state = state.clone();
-                new_state.build_robot(blueprint, material, time);
-                factory_states.push(new_state);
+                factory_states.push(future_state);
+                nb_new_states += 1;
             }
         }
+
+        // Terminal (leaf) state: not enough time left to build any new robots...
+        if nb_new_states == 0 {
+            // Collecting resources until the end
+            while current_state.time_remaining > 0 {
+                current_state.collect_resources();
+            }
+
+            // Updating best current production score
+            let max_score = current_state.geode_production_score();
+            if max_score > best_score {
+                best_score = max_score;
+            }
+
+            processed_states.push(current_state);
+        }
+
+        // Pruning all states which cannot produce more than the current best score value the end of duration...
+        factory_states = factory_states.drain(..).filter(|state| state.geode_production_score() >= best_score).collect();
     }
 
     // Qualifying blueprint quality
-    println!("Analyzing {} states...", factory_states.len());
-    if let Some(optimal_state) = factory_states.iter().max_by_key(|s| (s.resources[Material::Geode as usize], s.resources[Material::Obsidian as usize])) {
+    println!("Analyzing {} states...", processed_states.len());
+    if let Some(optimal_state) = processed_states.iter().max_by_key(|s| (s.resources[Material::Geode as usize], s.resources[Material::Obsidian as usize])) {
         optimal_state.print();
         blueprint.quality_level = blueprint.id as usize * optimal_state.resources[Material::Geode as usize];
     }
-
     println!("Quality level --> {}", blueprint.quality_level);
 }
 
@@ -316,6 +343,6 @@ mod tests {
     #[test]
     fn test_solution2() {
         let data = read_test_file();
-        assert_eq!((), solution2(data));
+        assert_eq!(56 * 62, solution2(data));
     }
 }
